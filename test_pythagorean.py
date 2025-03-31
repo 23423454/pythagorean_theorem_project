8d2786447a762d3546da3a5b24f7c830a2330585
23423454 <jrs-electro2014@yandex.ru>
2025-03-31 05:40:19 +0000

    Update test_pythagorean.py

--- test_pythagorean.py
+++ test_pythagorean.py
@@ -1,15 +1,15 @@
 import unittest
-from main import check_pythagorean
+from main import check_pythagorean  
 
-class TestPythagoreanTheorem(unittest.TestCase):
-    def test_pythagorean_true(self):
+class TestPythagorean(unittest.TestCase):
+    def test_valid_triplet(self):
         self.assertTrue(check_pythagorean(3, 4, 5))
-    
-    def test_pythagorean_false(self):
-        self.assertFalse(check_pythagorean(5, 5, 10))
 
-    def test_pythagorean_another_true(self):
-        self.assertTrue(check_pythagorean(6, 8, 10))
+    def test_invalid_triplet(self):
+        self.assertFalse(check_pythagorean(3, 4, 6))
+
+    def test_zero_values(self):
+        self.assertFalse(check_pythagorean(0, 0, 0))
 
 if __name__ == '__main__':
     unittest.main()
